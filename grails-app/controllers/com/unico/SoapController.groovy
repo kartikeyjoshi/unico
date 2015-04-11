4d87a43d00bb447b0952913fbe4af5cc23eee426
KartikeyJoshi <kartikey@intelligrape.com>
2015-04-12 02:31:33 +0530

    Refactored code for Soap Controller and Service.

--- grails-app/controllers/com/unico/SoapController.groovy
+++ grails-app/controllers/com/unico/SoapController.groovy
@@ -6,19 +6,19 @@ class SoapController {
 
     def soapService
 
-    def index() {
-        render(soapService.gcdList() as JSON)
-    }
-
-    def gcd() {
-        render soapService.gcd()
-    }
-
-    def gcdList() {
-        render(soapService.gcdList() as JSON)
-    }
-
-    def gcdSum() {
-        render soapService.gcdSum()
-    }
+//    def index() {
+//        render(soapService.gcdList() as JSON)
+//    }
+//
+//    def gcd() {
+//        render soapService.gcd()
+//    }
+//
+//    def gcdList() {
+//        render(soapService.gcdList() as JSON)
+//    }
+//
+//    def gcdSum() {
+//        render soapService.gcdSum()
+//    }
 }
